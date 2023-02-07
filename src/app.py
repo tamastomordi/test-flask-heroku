c7072a6bef1402a7d1e3b7cc548459f3ec1bcc3f
Tamás Tömördi <tomordi.tamas@gmail.com>
2023-02-07 15:26:31 +0100

    second

--- src/app.py
+++ src/app.py
@@ -4,5 +4,9 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/')
+def home():
+   return "Hello World!"
+
 if __name__ == "__main__":
    app.run(debug=True)
