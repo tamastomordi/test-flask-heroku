8aa645c2199a3506d35e034e4f4a632111329c7a
Tamás Tömördi <tomordi.tamas@gmail.com>
2023-02-07 15:37:58 +0100

    smth

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/')
-def home():
-   return "Hello World!"
+@app.route('/', methods=['GET'])
+def get_data():
+   data = [
+      {'id': 1, 'year': 2000},
+      {'id': 2, 'year': 2005},
+      {'id': 3, 'year': 2021},
+   ]
+   return data
 
 if __name__ == "__main__":
    app.run(debug=True)
